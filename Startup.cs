6d866bc71422fabe9a780fe0133c64d43a63768f
yuichi ito <yito86@gmail.com>
2021-08-03 11:16:09 +0900

    a
    a

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace hello
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello CNB: .Net Core!!");
+                });
+            });
+        }
+    }
+}
